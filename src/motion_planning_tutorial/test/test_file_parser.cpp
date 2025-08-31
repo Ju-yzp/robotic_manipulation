b5f357d4610e90adff6c2229215d8262725ccfdb
Ju-yzp <wjpzy20230551@qq.com>
2025-08-31 23:37:25 +0800

    增加通过参数文件读取机械臂约束与包络体集合

--- src/motion_planning_tutorial/test/test_file_parser.cpp
+++ src/motion_planning_tutorial/test/test_file_parser.cpp
@@ -2,12 +2,13 @@
 
 #include <iostream>
 
-int main(){
-    std::string file_path = "/home/up/robotics-manipulation/src/motion_planning_tutorial/config/ur5e_description.yaml";
+int main() {
+    std::string file_path =
+        "/home/up/robotics-manipulation/src/motion_planning_tutorial/config/ur5e_description.yaml";
     namespace mpt = motion_planning_tutorial;
     mpt::RobotDescription rd(file_path, nullptr);
-    if(rd.isOverJointPositionLimit(2.5, "elbow")){
-        std::cout<<"This joint position is over the limit!"<<std::endl;
+    if (rd.isOverJointPositionLimit(2.5, "elbow")) {
+        std::cout << "This joint position is over the limit!" << std::endl;
     }
     return 0;
 }

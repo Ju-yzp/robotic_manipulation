7efebc37a7ec18865ea7d429f36d1b536cbb25d0
Ju-yzp <wjpzy20230551@qq.com>
2025-09-27 18:33:17 +0800

    update:  NearestNeighbor完成测试版本

--- include/fast_motion_planning/NearestNeighbor.hpp
+++ include/fast_motion_planning/NearestNeighbor.hpp
@@ -35,8 +35,7 @@ public:
 
     // 分配内存
     T* allocate() {
-        while (flag.test_and_set(std::memory_order_acquire))
-            ;
+        while (flag.test_and_set(std::memory_order_acquire)) std::this_thread::yield();
         if (id_stack_.empty()) return nullptr;
 
         uint32_t offset = id_stack_.top();
@@ -48,8 +47,7 @@ public:
     void free(void* object) {
         T* ptr = (T*)object;
         uint32_t offset = ptr - memory_ptr_;
-        while (flag.test_and_set(std::memory_order_acquire))
-            ;
+        while (flag.test_and_set(std::memory_order_acquire)) std::this_thread::yield();
         if (offset < capacity_) id_stack_.emplace(offset);
         flag.clear(std::memory_order_release);
     }
@@ -83,7 +81,7 @@ private:
         KdTreeNode* right_child{nullptr};  // 右孩子节点
         KdTreeNode* left_child{nullptr};   // 左孩子节点
         float alpha_bal;                   // 平衡系数
-        double radius;                     // 从节点张成的球型空间
+        double radius{0.0};                // 从节点张成的球型空间
     };
 
     // 两个点在每个轴的差必须都大于这个值，否则认为是同一个点
@@ -138,7 +136,7 @@ private:
     // 用于主线程存储重建树节点数据
     DataVector pcl_storage_;
 
-    // 释放以root作为树的根节点的整颗树的内存
+    // 释放以root作为树的根节点的整颗树的内存，非线程安全
     void freeTree(KdTreeNode* root) {
         if (!root) return;
 
@@ -255,19 +253,26 @@ private:
     }
 
     // 从该叶子节点出发，回溯至整颗kd-tree的根节点,更新全部被影响的节点
-    void update(KdTreeNode* node, bool allow_rebuild) {
+    void update(KdTreeNode* node, bool allow_rebuild, int diff) {
         if (!node) return;
 
         KdTreeNode* parent = node->parent;
+        KdTreeNode** rt{nullptr};
         while (parent) {
-            parent->tree_size += 1;
-            if (allow_rebuild && criterionCheck(parent)) rebuild(&parent);
+            parent->tree_size += diff;
+            if (calc_dist(parent->data.point, node->data.point) > parent->radius)
+                parent->radius = calc_dist(parent->data.point, node->data.point);
+            if (allow_rebuild && criterionCheck(parent) &&
+                parent->tree_size > min_unbalanced_tree_size_)
+                rt = &parent;
             parent = parent->parent;
         }
+
+        if (rt && *rt) rebuild(rt);
     }
 
     // 添加点至kd-tree中
-    void add_point(Data data, KdTreeNode* root) {
+    void add_point(Data data, KdTreeNode* root, bool allow_rebuild = true) {
         if (!root) return;
 
         KdTreeNode* parent = root;
@@ -275,11 +280,10 @@ private:
         while (parent) {
             if (rebuild_tree_ && *rebuild_tree_ == parent) {
                 while (rebuild_flag_.test_and_set(std::memory_order_acquire))
-                    ;
-
+                    std::this_thread::yield();
                 pending_queue_.push(data);
                 rebuild_flag_.clear(std::memory_order_release);
-                return;  // 如果是把数据放入缓存队列中给后台重构建树进程，插入新的树中，就直接退出函数
+                return;
             } else {
                 if (cfv_[parent->div_axis](parent->data, data)) {
                     if (parent->right_child) {
@@ -289,7 +293,7 @@ private:
                     node = &parent->right_child;
                     break;
                 } else {
-                    if (parent->right_child) {
+                    if (parent->left_child) {
                         parent = parent->left_child;
                         continue;
                     }
@@ -302,35 +306,36 @@ private:
         // 申请内存
         *node = fmp_.allocate();
         (*node)->data = data;
-        update(*node, true);
+        (*node)->div_axis = (parent->div_axis + 1) % 3;
+        (*node)->parent = parent;
+        update(*node, allow_rebuild, 1);
     }
 
     void mutilRebuild() {
         while (!termination_flag_.load(std::memory_order_acquire)) {
             std::unique_lock<std::mutex> rebuild_lock(rebuild_mutex);
-            if (rebuild_tree_) {
+            if (rebuild_tree_ && *rebuild_tree_) {
+                while (rebuild_flag_.test_and_set(std::memory_order_acquire)) continue;
                 KdTreeNode* parent = (*rebuild_tree_)->parent;  // 记录父节点
-                KdTreeNode** new_root_node{nullptr};            // 记录新的点
+                KdTreeNode* node{nullptr};
+                KdTreeNode** new_root_node = &node;  // 记录新的点
                 KdTreeNode* old_root_node = *rebuild_tree_;
-
+                uint32_t old_tree_size = (*rebuild_tree_)->tree_size;
                 flatten(old_root_node, rebuild_pcl_storage_);
                 buildTree(new_root_node, rebuild_pcl_storage_);
-
-                while (rebuild_flag_.test_and_set(std::memory_order_acquire))
-                    ;
-
                 int num{0};
                 while (!pending_queue_.empty()) {
                     Data data = pending_queue_.front();
                     pending_queue_.pop();
                     ++num;
-                    add_point(data, *new_root_node);
+                    add_point(data, *new_root_node, false);
                     rebuild_flag_.clear(std::memory_order_release);
-                    if (num % 40 == 0) std::this_thread::sleep_for(std::chrono::microseconds(50));
+                    if (num % 40 == 0) std::this_thread::sleep_for(std::chrono::microseconds(70));
                     while (rebuild_flag_.test_and_set(std::memory_order_acquire))
                         ;
                 }
                 rebuild_flag_.clear(std::memory_order_release);
+
                 // 首先自旋等待没有搜索最邻近点，等待没有的时候,用新树替换掉旧树，并释放旧树申请的内存
                 int expected_counter = 0;
                 while (true) {
@@ -342,6 +347,7 @@ private:
                     expected_counter = 0;
                     std::this_thread::yield();
                 }
+
                 // 判断属于父节点的右子节点还是左子节点，然后使得指向正确的内存，并赋值子节点指向正确的父节点
                 if (parent) {
                     if (parent->right_child == old_root_node) parent->right_child = *new_root_node;
@@ -349,11 +355,13 @@ private:
                     (*new_root_node)->parent = parent;
                 }
                 search_state_counter_.store(0, std::memory_order_release);
-                freeTree(old_root_node);
-                update(*new_root_node, true);
+                update(*new_root_node, true, (*new_root_node)->tree_size - old_tree_size);
                 rebuild_tree_ = nullptr;
+
+                rebuild_lock.unlock();
+                freeTree(old_root_node);
+                std::cout << "Finish rebuild tree" << std::endl;
             }
-            rebuild_lock.unlock();
 
             // 休眠一段时间
             std::this_thread::sleep_for(std::chrono::microseconds(100));
@@ -408,42 +416,43 @@ private:
         }
 
         // 在serach_state_counter为-1时，不能进行搜索，但是serach_state_counter非0时，就会进行自增操作
-        // while (true && rebuilding_tree) {
-        //     if (search_state_counter_.compare_exchange_strong(
-        //             REBUILDING_FLAG, 0, std::memory_order_acq_rel, std::memory_order_acquire))
-        //         continue;
-        //     else if (search_state_counter_.compare_exchange_strong(
-        //                  REBUILDING_FLAG, 0, std::memory_order_acq_rel,
-        //                  std::memory_order_acquire)) {
-        //         search_by_tree(rebuilding_tree, point, nearest_node);
-        //         search_state_counter_.fetch_sub(1, std::memory_order_release);
-        //         break;
-        //     }
-        //     std::this_thread::yield();
-        // }
+        while (true && rebuilding_tree) {
+            if (search_state_counter_.compare_exchange_strong(
+                    REBUILDING_FLAG, 0, std::memory_order_acq_rel, std::memory_order_acquire))
+                continue;
+            else {
+                std::cout << "wow" << std::endl;
+                search_state_counter_.fetch_add(1, std::memory_order_release);
+                search_by_tree(rebuilding_tree, point, nearest_node, nearest_dist);
+                search_state_counter_.fetch_sub(1, std::memory_order_release);
+                break;
+            }
+            std::this_thread::yield();
+        }
     }
 
     // 搜索以root为根节点的树
-    // void search_by_tree(KdTreeNode* root, PointType point, Data& nearest_node) {
-    //     std::stack<KdTreeNode*> ns;
-    //     ns.emplace(root);
+    void search_by_tree(
+        KdTreeNode* root, PointType point, Data& nearest_node, double nearest_dist) {
+        std::stack<KdTreeNode*> ns;
+        ns.emplace(root);
 
-    //     while (!ns.empty()) {
-    //         KdTreeNode* node = ns.top();
-    //         ns.pop();
+        while (!ns.empty()) {
+            KdTreeNode* node = ns.top();
+            ns.pop();
+            double current_dist = calc_dist(node->data.point, point);
 
-    //         double cur_dist_sqr = calc_center_dist(node, point);
-    //         double radius_sqr = calc_dist(nearest_node.point, point);
-    //         if (radius_sqr + node->radius_sqr < cur_dist_sqr) continue;
+            if (current_dist < nearest_dist) {
+                nearest_node = node->data;
+                nearest_dist = current_dist;
+            }
 
-    //         if (node->right_child) ns.emplace(node->right_child);
-    //         if (node->left_child) ns.emplace(node->left_child);
-    //         double dist = calc_dist(node->data.point, point);
+            if (current_dist - node->radius > nearest_dist) continue;
 
-    //         if (dist > radius_sqr) continue;
-    //         nearest_node = node->data;
-    //     }
-    // }
+            if (node->right_child) ns.emplace(node->right_child);
+            if (node->left_child) ns.emplace(node->left_child);
+        }
+    }
 
     // 计算两个点的平方距离
     double calc_dist(PointType a, PointType b) { return (a - b).norm(); }
@@ -461,8 +470,14 @@ private:
         if (root->tree_size <= min_unbalanced_tree_size_) return false;
 
         double balance_evaluation = 0.0f;
-        KdTreeNode* child_ptr = root->left_child;
-        if (!child_ptr) child_ptr = root->right_child;
+        KdTreeNode* child_ptr{nullptr};
+        if (root->left_child)
+            child_ptr = root->left_child;
+        else if (root->right_child)
+            child_ptr = root->right_child;
+        else
+            return false;
+
         balance_evaluation = double(child_ptr->tree_size) / double(root->tree_size);
 
         if (balance_evaluation > balance_criterion_param_ ||
@@ -479,16 +494,18 @@ private:
         if ((*root)->tree_size >= mutil_rebuild_point_num_) {
             // 拿不到锁就选择等待下一次节点更新时触发重建
             std::unique_lock<std::mutex> rebuild_lock(rebuild_mutex, std::try_to_lock);
-            if (rebuild_lock.owns_lock())
-                if (rebuild_tree_ == nullptr || (*root)->tree_size > (*rebuild_tree_)->tree_size)
+            if (rebuild_lock.owns_lock()) {
+                if (rebuild_tree_ && *rebuild_tree_ &&
+                    (*rebuild_tree_)->tree_size < (*root)->tree_size)
                     rebuild_tree_ = root;
+                if (!rebuild_tree_) rebuild_tree_ = root;
+            }
         } else {
-            parent = (*root)->parent;
-            pcl_storage_.clear();
-            flatten(*root, pcl_storage_);
-            freeTree(*root);
-            buildTree(root, pcl_storage_);
-            if (!(*root)) (*root)->parent = parent;
+            KdTreeNode* old_root_node = *root;     // 记录旧树的根节点指向的内存位置
+            pcl_storage_.clear();                  // 清理容器
+            flatten(old_root_node, pcl_storage_);  // 收集旧树所有节点信息
+            buildTree(root, pcl_storage_);         // 构建旧树
+            freeTree(old_root_node);               // 释放旧树内存
         }
     }
 
@@ -521,6 +538,7 @@ public:
         freeTree(root_);
         root_ = nullptr;
     }
+
     // 搜索最邻近的节点
     void searchNearest(PointType point, Data& nearest_node) { search(point, nearest_node); }
 };
--- test/test_nearestNeighbors.cpp
+++ test/test_nearestNeighbors.cpp
@@ -1,5 +1,4 @@
 // cpp
-#include <algorithm>
 #include <chrono>
 #include <cstddef>
 #include <ratio>
@@ -7,22 +6,23 @@
 // fast_motion_planning
 #include <fast_motion_planning/NearestNeighbor.hpp>
 #include <fast_motion_planning/sampler.hpp>
+#include <thread>
 
 int main() {
     namespace fmp = fast_motion_planning;
     struct Data {
         Eigen::Vector<double, 6> point;
     };
-    fmp::NearestNeighbor<Data, 6, 20000> nn;
+    fmp::NearestNeighbor<Data, 6, 100000> nn;
     fmp::RandomNumberGenerator rng;
 
     auto start = std::chrono::high_resolution_clock::now();
     std::vector<Data> data_set;
 
     Data data;
-    for (size_t i{0}; i < 20000; ++i) {
+    for (size_t i{0}; i < 50000; ++i) {
         for (size_t j{0}; j < 6; ++j) {
-            data.point(j) = rng.uniform(11113.14, 22023.14);
+            data.point(j) = rng.uniform(2113.14, 22023.14);
         }
         data_set.emplace_back(data);
     }
@@ -32,62 +32,37 @@ int main() {
     std::cout << "KD树构建耗时:" << std::chrono::duration<double, std::milli>(end - start).count()
               << "ms\n";
 
-    data.point = Eigen::Vector<double, 6>{2000, 1000, 1250, 1000, 1000, 1000};
+    data.point = Eigen::Vector<double, 6>{12000, 41000, 6250, 8000, 11000, 10000};
 
-    start = std::chrono::high_resolution_clock::now();
-    auto point = data.point;
-    std::sort(data_set.begin(), data_set.end(), [point](auto& a, auto& b) {
-        return (a.point - point).transpose() * (a.point - point) <
-               (b.point - point).transpose() * (b.point - point);
-    });
-    // std::sort(data_set.begin(), data_set.end(), [point](auto a, auto b) {
-    //     return (a.point - point).norm() < (b.point - point).norm();
-    // });
-    end = std::chrono::high_resolution_clock::now();
-    std::cout << "暴力排序查询最邻近点:"
-              << std::chrono::duration<double, std::milli>(end - start).count() << "ms\n";
+    // nn.insert(data);
+    for (size_t i{0}; i < 20000; ++i) {
+        for (size_t j{0}; j < 6; ++j) {
+            data.point(j) = rng.uniform(-3113.14, 42023.14);
+        }
+        nn.insert(data);
+        // data_set.emplace_back(data);
+        //  start = std::chrono::high_resolution_clock::now();
+        auto point = data.point;
+        // std::sort(data_set.begin(), data_set.end(), [point](auto a, auto b) {
+        //     return (a.point - point).norm() < (b.point - point).norm();
+        // });
+        // end = std::chrono::high_resolution_clock::now();
+        // std::cout << "暴力排序查询最邻近点:"
+        //           << std::chrono::duration<double, std::milli>(end - start).count() << "ms\n";
 
-    start = std::chrono::high_resolution_clock::now();
-    nn.searchNearest(point, data);
-    end = std::chrono::high_resolution_clock::now();
-    std::cout << "KD树查询最邻近点:"
-              << std::chrono::duration<double, std::milli>(end - start).count() << "ms\n";
+        start = std::chrono::high_resolution_clock::now();
+        nn.searchNearest(point, data);
+        end = std::chrono::high_resolution_clock::now();
+        std::cout << "KD树查询最邻近点:"
+                  << std::chrono::duration<double, std::milli>(end - start).count() << "ms\n";
 
-    Eigen::IOFormat CleanFmt(4, 0, ", ", "\n", "[", "]");
-    std::cout << "暴力排序最邻近点" << std::endl;
-    std::cout << data_set[0].point.format(CleanFmt) << std::endl;
-    std::cout << "Kd-tree搜索最邻近点" << std::endl;
-    std::cout << data.point.format(CleanFmt) << std::endl;
-    // for (int i{0}; i < 70000; ++i) {
-    //     for (size_t j{0}; j < 6; ++j) {
-    //         point(j) = rng.uniform(2113.14, 1223.14);
-    //     }
-    //     nn.add(point);
-    // }
-    // auto cur_time = std::chrono::high_resolution_clock::now();
-    // std::cout << "KD树插入点耗时:"
-    //           << std::chrono::duration<double, std::milli>(cur_time - end).count() << "ms\n";
+        Eigen::IOFormat CleanFmt(4, 0, ", ", "\n", "[", "]");
+        // std::cout << "暴力排序最邻近点" << std::endl;
+        // std::cout << data_set[0].point.format(CleanFmt) << std::endl;
+        std::cout << "Kd-tree搜索最邻近点" << std::endl;
+        std::cout << data.point.format(CleanFmt) << std::endl;
+        std::this_thread::sleep_for(std::chrono::milliseconds(1));
+    }
 
-    // point(0) = 2000.0f;
-    // point(1) = 1000.0f;
-    // point(2) = -200.0f;
-    // point(3) = 780.0f;
-    // point(4) = 50.0f;
-    // point(5) = -1000.0f;
-
-    // points.clear();
-
-    // std::vector<float> dist;
-    // start = std::chrono::high_resolution_clock::now();
-    // nn.searchNearest(point, 1, points, dist);
-
-    // for (auto nearest_point : points) {
-    //     Eigen::IOFormat CleanFmt(4, 0, ", ", "\n", "[", "]");
-    //     std::cout << std::endl;
-    //     std::cout << nearest_point.format(CleanFmt) << std::endl;
-    // }
-    // end = std::chrono::high_resolution_clock::now();
-    // std::cout << "查询耗时:" << std::chrono::duration<double, std::milli>(end - start).count()
-    //           << "ms\n";
-    // return 0;
+    return 0;
 }
